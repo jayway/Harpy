fe2a54869ba99212a41a6867efd4955016dfc982
Felix Hedlund <felix.hedlund@jayway.com>
2017-03-31 15:56:42 +0100

    animation fix

--- Harpy/IngvarView.swift
+++ Harpy/IngvarView.swift
@@ -23,8 +23,8 @@ class IngvarView: UIView {
     func startAnimating(){
         let bigScale: CGFloat = 1.3
         let bubbleDuration: TimeInterval = 0.65
-        let ingvarRemoveDuration: TimeInterval = 2.2
-        let removeDuration: TimeInterval = 3
+        let ingvarRemoveDuration: TimeInterval = 1
+        let removeDuration: TimeInterval = 2
 
         UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
             self.ingvarImage.transform = CGAffineTransform(scaleX: bigScale, y: bigScale)
@@ -38,12 +38,11 @@ class IngvarView: UIView {
                     UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
                         self.ingvarImage.transform = CGAffineTransform.identity
                     }) { (completed) in
-                        UIView.animate(withDuration: ingvarRemoveDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                        UIView.animate(withDuration: ingvarRemoveDuration, delay: 0, options: [.curveEaseOut], animations: {
                             self.ingvarImage.frame = CGRect(origin: CGPoint(x: self.frame.width/2, y: 0), size: CGSize(width: 0, height: 0))
                             self.ingvarImage.alpha = 0
                         }, completion: nil)
-                        
-                        UIView.animate(withDuration: removeDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                        UIView.animate(withDuration: removeDuration, delay: 0.5, options: [.curveEaseOut], animations: {
                             self.alpha = 0
                         }, completion: { (completed) in
                             self.removeFromSuperview()
