0c73377a06367706c83d7fea920699e16fb5a8ba
Felix Hedlund <felix.hedlund@jayway.com>
2017-03-31 15:38:47 +0100

    Fixed animation for Ingvar

--- Harpy/IngvarView.swift
+++ Harpy/IngvarView.swift
@@ -21,17 +21,36 @@ class IngvarView: UIView {
     }
     
     func startAnimating(){
-        UIView.animate(withDuration: 0.5, delay: 0, options: [.repeat, .autoreverse, .curveEaseInOut], animations: {
-            UIView.setAnimationRepeatCount(3)
-            self.ingvarImage.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        let bigScale: CGFloat = 1.3
+        let bubbleDuration: TimeInterval = 0.65
+        let ingvarRemoveDuration: TimeInterval = 2.2
+        let removeDuration: TimeInterval = 3
+        
+        UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
+            self.ingvarImage.transform = CGAffineTransform(scaleX: bigScale, y: bigScale)
         }) { (completed) in
-            UIView.animate(withDuration: 3, animations: {
-                self.ingvarImage.frame = CGRect(origin: CGPoint(x: self.frame.width/2, y: 0), size: CGSize(width: 0, height: 0))
-                self.ingvarImage.alpha = 0
-                self.alpha = 0
-            }, completion: { (completed) in
-                self.removeFromSuperview()
-            })
+            UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                self.ingvarImage.transform = CGAffineTransform.identity
+            }) { (completed) in
+                UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                    self.ingvarImage.transform = CGAffineTransform(scaleX: bigScale, y: bigScale)
+                }) { (completed) in
+                    UIView.animate(withDuration: bubbleDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                        self.ingvarImage.transform = CGAffineTransform.identity
+                    }) { (completed) in
+                        UIView.animate(withDuration: ingvarRemoveDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                            self.ingvarImage.frame = CGRect(origin: CGPoint(x: self.frame.width/2, y: 0), size: CGSize(width: 0, height: 0))
+                            self.ingvarImage.alpha = 0
+                        }, completion: nil)
+                        
+                        UIView.animate(withDuration: removeDuration, delay: 0, options: [.curveEaseInOut], animations: {
+                            self.alpha = 0
+                        }, completion: { (completed) in
+                            self.removeFromSuperview()
+                        })
+                    }
+                }
+            }
         }
     
         
